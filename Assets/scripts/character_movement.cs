8d15548e9b0fa008aefd00dafaeaf44da96ddeab
APOLLO\gameb <189248david@gmail.com>
2026-03-04 22:11:55 -0800

    jump

--- Assets/scripts/character_movement.cs
+++ Assets/scripts/character_movement.cs
@@ -5,13 +5,15 @@ using UnityEngine.InputSystem;
 public class character_movement : MonoBehaviour
 {
     [SerializeField] private Transform cameraTransform;
+  
 
     private CharacterController controller;
     public float jumpHeight = 2f;
     public float speed = 5f;
-    public float gravity = -9.81f;
+    private float gravity = -9.81f;
     [SerializeField] private float gravityMultiplier = 3f;
     private float velocityY;
+    private float velocity;
     [SerializeField] private bool shouldFaceMove = false;
 
     public LayerMask groundLayer;
@@ -25,7 +27,7 @@ public class character_movement : MonoBehaviour
     private float invinciblity;
     public float invincibiltyTime;
 
-    private Vector2 input;
+    private Vector2 moveInput;
     private Vector3 _direction;
     private void Awake()
     {
@@ -41,58 +43,66 @@ public class character_movement : MonoBehaviour
     void Update()
     {
 
-        ApplyMovement();
         ApplyGravity();
-        Jump(new InputAction.CallbackContext());
 
-        if (shouldFaceMove &&_direction.sqrMagnitude > 0.001f)
-        {
-           Quaternion toRotation = Quaternion.LookRotation(_direction, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 10f);
-        }
-
-        invinciblity -= Time.deltaTime;
-
-    }
-
-    public void ApplyMovement()
-    {
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
+
         forward.y = 0;
         right.y = 0;
 
         forward.Normalize();
         right.Normalize();
-       
-        Vector3 moveDirection = forward * input.y + right * input.x;
-        controller.Move(moveDirection * speed * Time.deltaTime);
-    }
 
-    public void Move(InputAction.CallbackContext context)
-    {
-        input = context.ReadValue<Vector2>();
-       _direction = new Vector3(input.x, 0, input.y);
-    }
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
+        Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+
+        if (shouldFaceMove && moveDirection.sqrMagnitude > 0.001f)
         {
-            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity * gravityMultiplier);
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 10f);
         }
+
+        velocityY += gravity * gravityMultiplier * Time.deltaTime;
+        controller.Move(new Vector3(0, velocityY, 0) * Time.deltaTime);
+
+
+
+
+        invinciblity -= Time.deltaTime;
+
     }
+   
 
     private void ApplyGravity()
     {
-        if (IsGrounded())
+        if (controller.isGrounded && velocityY < 0.0f)
         {
-            velocityY = 0f;
+            velocity = -1.0f;
+
         }
-        else
+        else 
         {
-            velocityY += gravity * gravityMultiplier * Time.deltaTime;
-            controller.Move(new Vector3(0, velocityY, 0) * Time.deltaTime);
+            velocity+=gravity * gravityMultiplier * Time.deltaTime;
+            _direction.y = velocity;
         }
+    }
+    public void ApplyMovement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        _direction = new Vector3(moveInput.x, 0, moveInput.y);
+
+       
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+       if (context.started && controller.isGrounded)
+       {
+            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity * gravityMultiplier);
+       }
+
+
     }
     public void TakeDamage(float damage)
     {
@@ -131,7 +141,8 @@ public class character_movement : MonoBehaviour
         Debug.Log("Player has died.");
     }
 
-
+    private bool isGrounded() => controller.isGrounded;
+    
 
 
 }
